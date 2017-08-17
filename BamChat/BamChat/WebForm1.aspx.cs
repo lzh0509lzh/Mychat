0e391a549f5bc9b3673d718303778fb091c62e44
unknown <Administrator@MININT-RFH721G.fareast.corp.microsoft.com>
2017-08-17 17:20:42 +0800

    Nuget

---
+++ BamChat/BamChat/WebForm1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BamChat.myRedis;
+
+namespace BamChat
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RedisHelper.CreateClient("104.224.167.94", 26935, "Ms2688");
+            RedisHelper.setvaluestring("sbc", "Success");
+            string a = RedisHelper.getValueString("sbc");
+            Response.Write(a);
+        }
+    }
+}
---
+++ BamChat/BamRedis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace BamRedis
+{
+    class Program
+    {
+        private static RedisClient redisCli = new RedisClient("104.224.167.94", 6379,"Ms2688");
+
+        static void Main(string[] args)
+        {
+            bool res = redisCli.Set("city", "beijing");
+            if (res == true)
+            {
+                Console.WriteLine("success!");
+                Console.WriteLine("输入你要保存的key值：");
+                string w1 = Console.ReadLine();
+                Console.WriteLine("输入你要保存的value值：");
+                string w2 = Console.ReadLine();
+                bool res2 = redisCli.Set(w1, w2);
+                if (res2)
+                {
+                    Console.WriteLine("已保存：" + w1 + "," + redisCli.Get<string>(w1));
+                }
+                else
+                {
+                    Console.WriteLine("保存失败！");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
